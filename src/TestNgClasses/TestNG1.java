e4c05b2d28e4a43223aa861c85ff4e35fd112164
Karishma <karishmakate31@gmail.com>
2025-01-04 21:06:26 +0530

    Selenium Practice.

---
+++ src/TestNgClasses/TestNG1.java
@@ -0,0 +1,55 @@
+package TestNgClasses;
+
+import org.testng.annotations.*;
+
+public class TestNG1 {
+
+	@Test //reduce the dependency of java main method.
+	public void a2() {
+		System.out.println("Hey,a2 Test method...");
+	}
+
+	@BeforeTest 
+	public void b() {
+		System.out.println("Hey,BeforeTest method...");
+	}
+
+	@BeforeSuite
+	public void c() {
+		System.out.println("Hey,Before Suite method...");
+	}
+
+	@AfterSuite
+	public void d() {
+		System.out.println("Hey, After suite method...");
+	}
+
+	@BeforeMethod
+	public void e() {
+		System.out.println("Hey,Before method...");
+	}
+
+	@BeforeClass
+	public void f() {
+		System.out.println("Hey,Before Class method...");
+	}
+
+	@AfterTest 
+	public void g() {
+		System.out.println("Hey,After test method...");
+	}
+
+	@AfterClass
+	public void h() {
+		System.out.println("Hey,Afte Class method...");
+	}
+	@AfterMethod
+	public void i() {
+		System.out.println("Hey,After method...");
+	}
+
+	@Test //reduce the dependency of java main method.
+	public void a1() {
+		System.out.println("Hey,a1 Test method...");
+	}
+}
