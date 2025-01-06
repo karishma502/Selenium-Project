abeb3986fff63161834230a562159ba7ef2bfedc
Karishma <karishmakate31@gmail.com>
2025-01-06 23:39:38 +0530

    Selenium Practice.

--- src/TestNgClasses/TakeScreenshotsOnFailure.java
+++ src/TestNgClasses/TakeScreenshotsOnFailure.java
@@ -1,36 +1,37 @@
 package TestNgClasses;
 
 import java.io.File;
-
+import java.io.IOException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
-import org.testng.annotations.*;
+
+import com.google.common.io.Files;
 
 public class TakeScreenshotsOnFailure {
 
 	WebDriver driver;
-	@Test
-	public void captureScreenshot(ITestResult result) {
-		
+
+	public void captureScreenshot(ITestResult result) throws IOException {
+
 		driver = new ChromeDriver();
 		if(ITestResult.FAILURE== result.getStatus()) {
-			
+
 			//Create ref of takescreenshot interface and type casting
 			TakesScreenshot tc = (TakesScreenshot) driver;
-			
+
 			//use getScreenshot() to capture the screenshot in file formate
-			
+
 			File src = tc.getScreenshotAs(OutputType.FILE);
-			
+
 			//copy the file to specific location 
-		
-			File destination = new File("./screenshots" + result.getName()+ ".png");
-			
-			FileUtils.copyFile(src, destination);
-			
+
+			File destination = new File("./screenshots/" + result.getName()+ ".png");
+
+			Files.copy(src,destination);
+			System.out.println(result.getName()+"method screenshot captured.");
 		}
 	}
 }
---
+++ src/TestNgClasses/TakeScreenshotsOnFailure2.java
@@ -0,0 +1,47 @@
+package TestNgClasses;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.logging.FileHandler;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+
+import com.google.common.io.Files;
+
+public class TakeScreenshotsOnFailure2 {
+
+	TakeScreenshotsOnFailure t1 = new TakeScreenshotsOnFailure();
+
+	@Test
+	public void doLogin() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+		t1.driver = new ChromeDriver();
+		t1.driver.manage().window().maximize();
+		t1.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		t1.driver.get("https://www.facebook.com/");
+		Thread.sleep(2000);
+		Thread.sleep(2000);
+		t1.driver.findElement(By.id("email")).sendKeys("abc@123");
+		Thread.sleep(2000);
+		t1.driver.findElement(By.id("pass")).sendKeys("pass");
+
+		//pass wrong id so tht test will fail
+		t1.driver.findElement(By.id("email_wrong")).sendKeys("abc@123");
+	}
+
+	@AfterMethod
+	public void takeScreenshot(ITestResult result) throws IOException {
+
+		t1.captureScreenshot(result);
+	}
+
+
+
+}
